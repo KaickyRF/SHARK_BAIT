79f0b9dac56508877c8c614787859b06a0b6d9af
KaickyRF <kaickyferreira01@gmail.com>
2026-08-10 22:56:23 -0300

    feat: create Deal model in models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from database import Base
+from sqlalchemy import Column, Float, Integer, String
+
+class Deal(Base):
+    __tablename__= "deals"
+
+    id = Column(Integer, primary_key=True, index=True)
+    dealID = Column(String, unique=True, index=True, nullable=False)
+
+    title = Column(String, index=True, nullable=False)
+    shop = Column(String, nullable=False)
+    price_now = Column(Float, nullable=False, index=True)
+    normal_price = Column(Float, nullable=False)
+    metacritic = Column(Float, nullable=True)
+    steam_rate = Column(String, nullable=True)
+    steam_rate_percent = Column(Float, nullable=True)
+    critic_steam = Column(Float, nullable=False)
+    sort_rate_price = Column(Float, nullable=False, index=True)
+    
