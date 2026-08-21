247b40f8022f495471b89a3fd60859ed0d7a2358
KaickyRF <kaickyferreira01@gmail.com>
2026-08-20 22:38:10 -0300

    feat: add cors, auto create table in api and static files

--- api.py
+++ api.py
@@ -1,13 +1,27 @@
 import models
 import schemas
 
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, Depends
-from database import SessionLocal
+
+from database import SessionLocal, engine
 from typing import List
 from sqlalchemy.orm import Session
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SharkBait API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 def get_database():
     db = SessionLocal()
